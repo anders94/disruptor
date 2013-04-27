6d4653a20706990a6221509367227419fc7021ca
Anders Brownworth <anders-git@evantide.com>
2013-04-27 15:15:18 -0400

    dynamic number of sub=processes bount to number of cpu cores

--- snips/cluster.js
+++ snips/cluster.js
@@ -1,13 +1,19 @@
 var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
 if (cluster.isMaster) {
-    cluster.fork();
-    cluster.fork();
+    console.log('forking '+numCPUs+' processes');
+    // Fork workers.
+    for (var i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 
-    cluster.on('disconnect', function(worker) {
-	    console.error('disconnect!');
-	    cluster.fork();
-	});
+    cluster.on('exit', function(worker, code, signal) {
+            console.log('ERROR! worker ' + worker.process.pid + ' died - attempting to restart');
+            setTimeout(function() {
+                    cluster.fork();
+                }, 1000); // wait 1 second before forking again
+        });
 }
 else {
     console.log("run");
